46b2eda6d9cf12013d7c3a7472b2561db4282966
giniblanco <blancogini@gmail.com>
2023-03-11 23:16:19 +0000

    speciesTile LWC.

---
+++ force-app/main/default/lwc/speciesTile/speciesTile.js
@@ -0,0 +1,13 @@
+import { LightningElement } from 'lwc';
+
+export default class SpeciesTile extends LightningElement {
+    species = {
+        Name: "Jazmin",
+        Description: "Olorosa y bonita planta trepadora",
+        Image_URL__c: "https://cdn.shopify.com/s/files/1/0513/3428/3424/collections/jasmine-5346865_1920_1200x1200.jpg?v=1664185794",
+        Location__c: "Indoors,Outdoors",
+
+    };
+}
+
+    
