9e6bae6e84ddb9d9095ba43fbc91fe9beb497588
giniblanco <blancogini@gmail.com>
2023-03-14 20:29:52 +0000

    lightning-input

--- force-app/main/default/lwc/speciesList/speciesList.js
+++ force-app/main/default/lwc/speciesList/speciesList.js
@@ -1,11 +1,27 @@
 import { LightningElement, wire } from 'lwc';
-import getAllSpec from '@salesforce/apex/SpeciesService.getAllSpecies'; //los métodos se pueden renombrar así
+import getFilteredSpec from '@salesforce/apex/SpeciesService.getFilteredSpecies'; //los métodos se pueden renombrar así
 
 export default class SpeciesList extends LightningElement {
-    @wire(getAllSpec)  //le decimos al Lightning Data Service que le delegamos la invocacion de este metodo
+    // PROPERTIES, GETTERS & SETTERS
+    searchText = "";
+
+    // LIFECYCLE HOOKS
+    
+
+    // WIRE
+    @wire(getFilteredSpec, { searchText : '$searchText'})  //le decimos al Lightning Data Service que le delegamos la invocacion de este metodo
     speciesFromDB;    //guarda los valores en esta variable/propiedad
 
     //speciesFromDB.data --> datos devueltos por Apex
     //speciesFromDB.error --> si hay error, se almacena aqui
+
+    // METHODS
+    handleInputChange(event) {
+        const searchTextAux = event.target.value;
+
+        if(searchTextAux.length >= 2 || searchTextAux === '') {
+            this.searchText = searchTextAux;
+        }
+    }
     
 }
